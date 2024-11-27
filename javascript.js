f305b7fceaec1394f7aa9e42296983f198548f6e
Valeria <160492192+valeria-living@users.noreply.github.com>
2024-11-27 12:15:13 +0000

    push it 🐶

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var cards = [
+    ["Question0","Answer0",false],
+    ["Question1","Answer1",false],
+    ["Question2","Answer2",false],
+    ["Question3","Answer3",false],
+    ["Question4","Answer4",false],
+    ["Question5","Answer5",false]
+]
